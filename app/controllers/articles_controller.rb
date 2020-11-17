6c1a0fb71c16aeea61520f4cf382a90ea0f07580
tahiry-dev <tahirycontact@gmail.com>
2020-11-17 15:19:51 +0300

    add action for index and show, and adjust the view accordingly

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,14 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        
+    end
+
 end
