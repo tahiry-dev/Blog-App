f65103c0ff25f84ba96c55a85d6891dcbcc8bceb
tahiry-dev <tahirycontact@gmail.com>
2020-11-18 14:56:21 +0300

    add the crud functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,45 @@ class ArticlesController < ApplicationController
     end
 
     def new
-        
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        respond_to  do |delete|
+            delete.html {redirect_to articles_url, notice: "The article was successfully deleted" }
+        end
+    end
+
+    def create
+        @create_item = Article.new(params.require(:article).permit(:title, :body))
+
+        respond_to (@article) do |create|
+            if @create_item.save
+                create.html {redirect_to @create_item, notice: 'The article has been created correctly'}
+                create.json { render :show, status: :created, location: @create_item }
+            else
+                create.html(render :new)
+                create.json { render json: @create_item.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def update
+        @update_item =  Article.find(params[:id])
+
+        respond_to (@article) do |update|
+            if @update_item.update(params.require(:article).permit(:title, :body))
+                update.html {redirect_to edit_article_path, notice: 'The article has been updated correctly'}
+            else
+                create.html(render :edit)
+            end
+        end
     end
 
 end
